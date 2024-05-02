5b0753554b3df6eba3ef462da853271abcb9b054
Emmanuel <emmanuelola961@gmail.com>
2024-05-02 14:53:04 +0100

    Trying to deploy 3

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -8,6 +8,7 @@ import EmailVerification from './Pages/EmailVerification'
 import Profile from './Pages/Profile'
 import Forgotten from './Pages/Forgotten'
 import AddMoney from './Pages/AddMoney'
+import Home from './Pages/Home'
 
 const App = () => {
   let token = localStorage.getItem('token')
@@ -15,14 +16,15 @@ const App = () => {
     <>
       <Toaster position='top-center' reverseOrder={false} />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/user">
           <Route path="/user/register" element={<SignUp />} />
           <Route path="/user/login" element={<Sign />} />
           <Route path="/user/reset_password" element={<Forgotten />} />
-          <Route path="/user/dashboard" element={ token ? <Dashboard /> : <Sign />} />
-          <Route path="/user/dashboard/profile" element={ token ? <Profile /> : <Sign />} />
-          <Route path="/user/dashboard/fund_wallet" element={ token ? <AddMoney /> : <Sign />} />
-          <Route path="/user/verify" element={ token ? <EmailVerification /> : <SignUp /> } />
+          <Route path="/user/dashboard" element={token ? <Dashboard /> : <Sign />} />
+          <Route path="/user/dashboard/profile" element={token ? <Profile /> : <Sign />} />
+          <Route path="/user/dashboard/fund_wallet" element={token ? <AddMoney /> : <Sign />} />
+          <Route path="/user/verify" element={token ? <EmailVerification /> : <SignUp />} />
         </Route>
       </Routes>
     </>
--- front end/src/Pages/Home.jsx
+++ front end/src/Pages/Home.jsx
@@ -1,1 +1,9 @@
-rafce
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home

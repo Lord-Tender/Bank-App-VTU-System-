f162846c20d1475abc3b0cbb51ae519cc87900f9
Emmanuel <emmanuelola961@gmail.com>
2024-05-06 11:18:31 +0100

    Frontend deployment 2

--- front end/src/Pages/Home.jsx
+++ front end/src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <>
+    <h1>Home page</h1>
       <div><Link to='/user/login'>Login</Link></div>
-      <div><Link to='/user/Register'>Login</Link></div>
+      <div><Link to='/user/Register'>Register</Link></div>
       <div><Link to='/user/reset_password'>Login</Link></div>
-      <div><Link to='/user/login'>Login</Link></div>
     </>
   )
 }

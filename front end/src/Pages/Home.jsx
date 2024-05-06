f6cf0b72907a5b18ab0d10e6e3f38ee7cdc18efe
Emmanuel <emmanuelola961@gmail.com>
2024-05-06 11:05:54 +0100

    Frontend deployment

--- front end/src/Pages/Home.jsx
+++ front end/src/Pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div><Link to='/user/login'>Login</Link></div>
+      <div><Link to='/user/Register'>Login</Link></div>
+      <div><Link to='/user/reset_password'>Login</Link></div>
+      <div><Link to='/user/login'>Login</Link></div>
+    </>
   )
 }
 

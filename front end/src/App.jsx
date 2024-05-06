d967335ac1070b02a3f89e23c07e75ec89788d35
Emmanuel <emmanuelola961@gmail.com>
2024-05-06 10:44:11 +0100

    Trying 6

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes, Navigate  } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import SignUp from './Pages/SignUp'
 import Sign from './Pages/SignIn'
 import Dashboard from './Pages/Dashboard'
@@ -11,7 +11,7 @@ import AddMoney from './Pages/AddMoney'
 import Home from './Pages/Home'
 
 const App = () => {
-  
+
   let token = localStorage.getItem('token')
 
   return (
@@ -19,15 +19,13 @@ const App = () => {
       <Toaster position='top-center' reverseOrder={false} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/user">
-          <Route path="/user/register" element={<SignUp />} />
-          <Route path="/user/login" element={<Sign />} />
-          <Route path="/user/reset_password" element={<Forgotten />} />
-          <Route path="/user/dashboard" element={token ? <Dashboard /> : <Navigate to="/user/login?redirected=true" />} />
-          <Route path="/user/dashboard/profile" element={token ? <Profile /> : <Navigate to="/user/login?redirected=true" />} />
-          <Route path="/user/dashboard/fund_wallet" element={token ? <AddMoney /> : <Navigate to="/user/login?redirected=true" />} />
-          <Route path="/user/verify" element={token ? <EmailVerification /> : <Navigate to="/user/login?redirected=true" />} />
-        </Route>
+        <Route path="/user/register" element={<SignUp />} />
+        <Route path="/user/login" element={<Sign />} />
+        <Route path="/user/reset_password" element={<Forgotten />} />
+        <Route path="/user/dashboard" element={token ? <Dashboard /> : <Navigate to="/user/login?redirected=true" />} />
+        <Route path="/user/dashboard/profile" element={token ? <Profile /> : <Navigate to="/user/login?redirected=true" />} />
+        <Route path="/user/dashboard/fund_wallet" element={token ? <AddMoney /> : <Navigate to="/user/login?redirected=true" />} />
+        <Route path="/user/verify" element={token ? <EmailVerification /> : <Navigate to="/user/login?redirected=true" />} />
       </Routes>
     </>
   )

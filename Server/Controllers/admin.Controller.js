524c37d9bf560dddcef2b9788d84e973db0b89c9
Emmanuel <emmanuelola961@gmail.com>
2024-05-15 14:34:43 +0100

    Add admin user code added

--- Server/Controllers/admin.Controller.js
+++ Server/Controllers/admin.Controller.js
@@ -1,1 +1,18 @@
 const { adminUser } = require("")
+
+const addAdminUser = (req, res) => {
+    const { email, role, fullName, password } = req.body
+    let admin = new adminUser({
+        email, 
+        role, 
+        fullName, 
+        password
+    })
+    admin.save()
+    .then((response)=>{
+        res.status(200).json({status: true, msg: "User added successfully"})
+    })
+    .catch((err)=>{
+        res.status(200).json({status: false, msg: "An error occured", error: err})
+    })
+}

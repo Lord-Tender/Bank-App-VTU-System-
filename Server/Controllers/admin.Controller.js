0bea35e893f3387febe17cedb140c764e7c57b53
Emmanuel <emmanuelola961@gmail.com>
2024-05-15 16:30:07 +0100

    Server: Credit user feature added

--- Server/Controllers/admin.Controller.js
+++ Server/Controllers/admin.Controller.js
@@ -7,30 +7,49 @@ const addAdminUser = (req, res) => {
     const { email, role, fullName, password } = req.body
     let hashPassword = bcrypt.hashSync(password, 10)
     let admin = new adminUser({
-        email, 
-        role, 
-        fullName, 
+        email,
+        role,
+        fullName,
         password: hashPassword
     })
     admin.save()
-    .then((response)=>{
-        console.log(response)
-        res.status(200).json({status: true, msg: "User added successfully"})
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.status(200).json({status: false, msg: "An error occured", error: err})
-    })
+        .then((response) => {
+            console.log(response)
+            res.status(200).json({ status: true, msg: "User added successfully" })
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(200).json({ status: false, msg: "An error occured", error: err })
+        })
 }
 
 const fetchAllUser = async (req, res) => {
     try {
         let allUsers = await userModel.find({})
         res.status(200).json({ status: true, allUsers })
-      } catch (err) {
+    } catch (err) {
         console.error(err);
         res.status(500).json({ status: false, error: err })
-      }
+    }
 }
 
-module.exports = { addAdminUser, fetchAllUser }
+const creditUser = async (req, res) => {
+    const { userEmail, amount } = req.body
+    let user = await userModel.findOne({ 'emailInfo.email': userEmail })
+    if (user) {
+        let oldBalance = Number(user.accountBal)
+        const newBalance = oldBalance + Number(amount)
+        user.accountBal = newBalance
+        user.save()
+        .then((response)=>{
+            res.status(200).json({msg: "Success", status: true, response})
+        })
+        .catch((err)=>{
+            res.status(400).json({msg: "unsuccessful", status: false, error: err})
+        })
+    } else {
+        res.status(500).json({ msg: "An error occured" })
+    }
+}
+
+module.exports = { addAdminUser, fetchAllUser, creditUser }
--- Server/Routes/admin.Route.js
+++ Server/Routes/admin.Route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { addAdminUser, fetchAllUser } = require("../Controllers/admin.Controller")
+const { addAdminUser, fetchAllUser, creditUser } = require("../Controllers/admin.Controller")
 
 router.post("/add_user", addAdminUser)
 router.get("/get_user", fetchAllUser)
+router.post("/credit_user", creditUser)
 
 module.exports = router;

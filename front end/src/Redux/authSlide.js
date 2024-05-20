f6bb5ea7bd2a77ad0c220fc5e6766461026d9364
Emmanuel <emmanuelola961@gmail.com>
2024-05-20 14:47:56 +0100

    Store and auth slide added

---
+++ front end/src/Redux/authSlide.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setAuthenticated: (state, action) => {
+      state.isAuthenticated = action.payload;
+    },
+  },
+});
+
+export const { setAuthenticated } = authSlice.actions;
+export default authSlice.reducer;

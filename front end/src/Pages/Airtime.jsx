b99dbbe93eeae52d24454a18fb5cd8f77d7cde5e
Emmanuel <emmanuelola961@gmail.com>
2024-06-18 10:59:29 +0100

    Airtime Sidebar & Sidebar added

--- front end/src/Pages/Airtime.jsx
+++ front end/src/Pages/Airtime.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Siderbar from '../Components/Siderbar'
+import Rightbar from '../Components/Rightbar'
 
 const Airtime = () => {
     return (
@@ -7,6 +8,18 @@ const Airtime = () => {
             <section id='section' className="mainSection flex relative w-full " >
 
                 <Siderbar />
+
+                {/* Main body */}
+
+                <div className='w-full lg:w-[60%] md:w-[60%] h-screen bg-gray-100 static lg:absolute px-7 sm:px-3 md:fixed left-[20%] ' style={{ fontFamily: '"Josefin Sans", sans-serif' }}>
+
+                    <div className='w-full bg-white h-[6em] my-3 rounded-xl'>
+
+                    </div>
+                </div>
+
+
+                <Rightbar />
 
             </section>
         </>

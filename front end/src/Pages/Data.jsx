91ae0cc0767d18a803cb10d6df72880147970ea5
Emmanuel <emmanuelola961@gmail.com>
2024-06-19 10:51:16 +0100

    feat(Data page): page auth added

--- front end/src/Pages/Data.jsx
+++ front end/src/Pages/Data.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Data = () => {
-  return (
-    <>
-        
-    </>
-  )
-}
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    let url = "http://localhost:5000/user/page_auth";
 
-export default Data
+    axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => {
+        setuser(res.data.userResult);
+        if (res.data.emailVerified == false) {
+          navigate("/user/not-verify");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.response.data.error.name == "TokenExpiredError") {
+          toast.error("Session expired!!!");
+          setTimeout(() => {
+            localStorage.removeItem("token");
+            navigate("/user/login");
+          }, 3000);
+        } else if (err) {
+          navigate("/user/login");
+        }
+      });
+  }, []);
+
+  return (
+    <></>
+  )
+};
+
+export default Data;

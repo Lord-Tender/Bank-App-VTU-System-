fe45ff16894bd33dd9048f6cc7e1ad3014320608
Emmanuel <emmanuelola961@gmail.com>
2024-06-19 13:59:07 +0100

    fix(Home page): section 1 & 2 resposiveness

--- front end/src/Pages/Home.jsx
+++ front end/src/Pages/Home.jsx
@@ -24,29 +24,29 @@ const Home = () => {
 
       {/* Section 1 */}
 
-      <div className="section2 w-full h-[30em] bg-slate-100 flex border-b-8 border-blue-300" style={{ fontFamily: '"Josefin Sans", sans-serif' }}>
-        <div className='w-hf flex mt-[-2em] items-center'>
+      <div className="section2 w-full h-[30em] bg-slate-100 flex border-b-8 border-blue-300 sm:flex-col" style={{ fontFamily: '"Josefin Sans", sans-serif' }}>
+        <div className='w-hf sm:w-full sm:mt-10 h-hf flex mt-[-2em] items-center'>
           <div className='ms-[10%]'>
-            <h1 className='text-[2.6em] text-bold text-blue-600 border-s-8 border-blue-600 ps-4 rounded-[5px]'>Bank Beyond Imagination</h1>
-            <p className='text-[3em] ps-6 text-red-500 '>Save, Send & Invest</p>
-            <h2 className='ps-6'><span className='text-blue-600 text-[1.7em]'> <i>Bank easy,</i></span><span className='text-red-500 text-[2.2em] '> <i>Live long.</i></span> </h2>
+            <h1 className='text-[2.6em] sm:text-[1.4em] text-bold text-blue-600 border-s-8 border-blue-600 ps-4 rounded-[5px]'>Bank Beyond Imagination</h1>
+            <p className='text-[3em] sm:text-[1em] ps-6 text-red-500 '>Save, Send & Invest</p>
+            <h2 className='ps-6'><span className='text-blue-600 text-[1.7em] sm:text-[1em]'> <i>Bank easy,</i></span><span className='text-red-500 text-[2.2em] sm:text-[1.3em]'> <i>Live long.</i></span> </h2>
             <Link to='/user/register'>
-              <button className='h-12 bg-sky-400 text-white font-semibold hover:bg-white hover:text-blue-950 w-[10em] rounded-3xl ms-6 mt-5'>Get Started</button>
+              <button className='h-12 bg-sky-400 text-white font-semibold hover:bg-white hover:text-blue-950 w-[10em] rounded-3xl ms-6 mt-5 sm:h-8 sm:text-[0.7em] sm:mt-3'>Get Started</button>
             </Link>
           </div>
         </div>
-        <div className='w-hf'>
+        <div className='w-hf sm:w-full sm:h-hf'>
           <img src={section1Img} alt="" className='w-full h-full ' />
         </div>
       </div>
 
       {/* Section  */}
 
-      <div className="section2 w-full h-[30em] bg-white flex" style={{ fontFamily: '"Josefin Sans", sans-serif' }}>
-        <div className='w-hf'>
+      <div className="section2 w-full h-[30em] bg-white flex sm:flex-col-reverse" style={{ fontFamily: '"Josefin Sans", sans-serif' }}>
+        <div className='w-hf sm:h-hf sm:w-full '>
           <img src={section2Img} alt="" className='w-full h-full ' />
         </div>
-        <div className='w-hf flex mt-[-4em] items-center border-blue-50'>
+        <div className='w-hf sm:w-full sm:h-hf flex mt-[-4em] items-center border-blue-50'>
           <div className='ms-[10%]'>
           <h1 className='text-[2.8em] text-bold border-blue-700 rounded-[5px]'>Fast Transfer</h1>
           <p className='mt-3 text-lg text-blue-700 w-[70%] '>Money movement made easy. Move your money anyway in ease at low transaction fee...</p>
